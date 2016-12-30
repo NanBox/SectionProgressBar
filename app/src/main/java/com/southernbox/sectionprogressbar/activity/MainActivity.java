e010cfb8dde7f869431c9ef3ce2a118880063c9f
LinNanQuan <linnq@zdnst.com>
2016-12-30 11:47:14 +0800

    first commit

---
+++ app/src/main/java/com/southernbox/sectionprogressbar/activity/MainActivity.java
@@ -0,0 +1,15 @@
+package com.southernbox.sectionprogressbar.activity;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.southernbox.sectionprogressbar.R;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
---
+++ app/src/main/java/com/southernbox/sectionprogressbar/widget/SectionProgress.java
@@ -0,0 +1,9 @@
+package com.southernbox.sectionprogressbar.widget;
+
+/**
+ * Created by nanquan.lin on 2016/12/30 0030.
+ */
+
+public class SectionProgress {
+
+}
