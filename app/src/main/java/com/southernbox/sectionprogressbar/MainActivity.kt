93290270ebceda88e7ce569fdad86184851aac31
LinNanQuan <k49220824@foxmail.com>
2017-11-08 10:43:11 +0800

    升级 Gradle

--- app/src/main/java/com/southernbox/sectionprogressbar/MainActivity.kt
+++ app/src/main/java/com/southernbox/sectionprogressbar/MainActivity.kt
@@ -1,10 +1,8 @@
-package com.southernbox.sectionprogressbar.activity
+package com.southernbox.sectionprogressbar
 
 import android.os.Bundle
 import android.support.v7.app.AppCompatActivity
 import android.view.View
-import com.southernbox.sectionprogressbar.R
-import com.southernbox.sectionprogressbar.widget.SectionProgressBar
 import kotlinx.android.synthetic.main.activity_main.*
 
 /**
--- app/src/main/java/com/southernbox/sectionprogressbar/SectionProgressBar.kt
+++ app/src/main/java/com/southernbox/sectionprogressbar/SectionProgressBar.kt
@@ -1,11 +1,10 @@
-package com.southernbox.sectionprogressbar.widget
+package com.southernbox.sectionprogressbar
 
 import android.animation.ObjectAnimator
 import android.content.Context
 import android.util.AttributeSet
 import android.view.LayoutInflater
 import android.widget.LinearLayout
-import com.southernbox.sectionprogressbar.R
 import kotlinx.android.synthetic.main.layout_section_progress.view.*
 
 /**
@@ -18,18 +17,6 @@ class SectionProgressBar @JvmOverloads constructor(context: Context,
                                                    defStyleAttr: Int = 0)
     : LinearLayout(context, attrs, defStyleAttr) {
 
-    init {
-        LayoutInflater.from(context).inflate(R.layout.layout_section_progress, this)
-    }
-
-    fun setProgress(stage: Float) {
-        val progressWidth = rootView.iv_progress.width
-        val animator = ObjectAnimator.ofFloat(rootView.iv_progress, "translationX", stage * progressWidth)
-        animator.duration = (Math.abs(stage - currentStage) * 1000).toInt().toLong()
-        animator.start()
-        currentStage = stage
-    }
-
     companion object {
         //不同阶段进度条所在位置
         val STAGE0 = 0f
@@ -40,6 +27,18 @@ class SectionProgressBar @JvmOverloads constructor(context: Context,
         val STAGE5 = 1f
         //当前阶段
         private var currentStage: Float = 0.toFloat()
+    }
+
+    init {
+        LayoutInflater.from(context).inflate(R.layout.layout_section_progress, this)
+    }
+
+    fun setProgress(stage: Float) {
+        val progressWidth = rootView.iv_progress.width
+        val animator = ObjectAnimator.ofFloat(rootView.iv_progress, "translationX", stage * progressWidth)
+        animator.duration = (Math.abs(stage - currentStage) * 1000).toInt().toLong()
+        animator.start()
+        currentStage = stage
     }
 
 }
